f3b96f61e34e7d14ed771a51c694e75004e8bc29
duonganh <duonganh2973@gmail.com>
2023-11-08 11:05:03 +0700

    init

---
+++ server/convertAccent.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+def no_accent_vietnamese(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    return s
+
+if __name__ == '__main__':
+    # print(no_accent_vietnamese("Việt Nam Đất Nước Con Người"))
+    source_path = os.path.join(os.path.dirname(__file__), 'data/map_org.osm')
+    dest_path = os.path.join(os.path.dirname(__file__), 'data/map.osm')
+    # convert content of file to another file
+    with open(source_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+        content = no_accent_vietnamese(content)
+        with open(dest_path, 'w') as f2:
+            f2.write(content)
+            f2.close()
+        f.close()
+    print("Done")
